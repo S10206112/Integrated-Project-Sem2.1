948c83059e43945f2ea37031f6e368767bf6c7ab
S10206112 <73154832+S10206112@users.noreply.github.com>
2021-08-12 23:08:24 +0800

    added alot of stuff

--- Assets/Scripts/SimpleEnemyHealth.cs
+++ Assets/Scripts/SimpleEnemyHealth.cs
@@ -7,13 +7,15 @@ public class SimpleEnemyHealth : MonoBehaviour
 {
 
     [SerializeField]
+    // enemy starting hp
     public int startingHealth;
-
+    //enemy current hp
     public int currentHealth;
+    //ref to the enemy object (used for boss)
     public GameObject EnemyName;
-    
+    //ref to the minioms that would spawn when boss is attacked (used for boss)
     public GameObject Minions;
-
+    //ref the player
     public playerEvent Player;
 
    
@@ -24,31 +26,39 @@ public class SimpleEnemyHealth : MonoBehaviour
     }
     private void OnEnable()
     {
-        
+        //enemy's starting health
         currentHealth = startingHealth;
     }
 
     
     
-
+    //take damage, ref from the shooting script
     public void TakeDamage(int damageAmount)
     {
+        //enemy health takes damage
         currentHealth -= damageAmount;
+        //debuglog enemy health
         Debug.Log(currentHealth);
+        //checks if the enemy that was shot is the boss (used only for boss)
         if (EnemyName.name == "PenguinBoss")
             {
+                //spawns little minions that attack the player
                 Instantiate(Minions, new Vector3(80, 100, 575), Quaternion.identity);
+                //debuglog minion spawned
                 Debug.Log("Minions spawned");
             }
 
         //if the enemy is dead
         if (currentHealth <= 0)
+            //triggers the Die() command
             Die();
     }
     
     private void Die()
     {
+        //trigger the add kill command in the player event script
         Player.GetComponent<playerEvent>().Addkills();
+        //enemy disappears
         gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/playerEvent.cs
+++ Assets/Scripts/playerEvent.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class playerEvent : MonoBehaviour
 {
+    //ref of the player's health
     public float PlayerHealth = 100;
 
+    //ref the character controller
     public CharacterController controller;
 
+    //ref to respawn point
     public Transform respawnPoint;
 
+    //ref for number of kills the player has
     public int PlayerKills = 0;
+
+    //to ref the penguin boss
+    public GameObject PenguinBoss;
 
     
 
@@ -23,15 +30,27 @@ public class playerEvent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if player health reaches zero
         if (PlayerHealth == 0)
         {
+            //debug that player is dead
             Debug.Log("Player has died");
+            //trigger the respawn function
             respawn();
         }
 
+        //Quest Completion conditions
+        //if the player gets 5 kills
         if (PlayerKills == 5)
         {
+            //debuglog
             Debug.Log("Quest 1 Completed : Kill 5 enemies");
+        }
+        // if the boss has been defeated
+        if (PenguinBoss.activeInHierarchy == false)
+        {
+            //debuglog
+            Debug.Log("Quest 5 Completed : Penguin Boss defeated");
         }
 
         
@@ -46,13 +65,16 @@ public class playerEvent : MonoBehaviour
             //player takes damage from the attack
             TakeDamage(10);
         }
+        //when player reaches the shelves section of the game
         if (other.gameObject.CompareTag("GoToShelves"))
         {
+            //debuglog
             Debug.Log("Quest 2 Complete : Go to Shelves");
-            
         }
+        //when player reaches the end of the maze
         if (other.gameObject.CompareTag("CompletedMaze"))
         {
+            //debuglog
             Debug.Log("Quest 3 Complete : Completed Maze");
             
         }
@@ -71,17 +93,23 @@ public class playerEvent : MonoBehaviour
         
     }
 
+    //respawn script
     void respawn()
     {
+        //debuglog
         Debug.Log("Player has died, returning to respawn point");
+        //resets player health to full
         PlayerHealth = 100;
+        //resets player position to respawn point
         controller.transform.position = respawnPoint.transform.position;
     }
 
+    //tallying the num of kills the player has
     public void Addkills()
     {
-        
+        //add a kill
         PlayerKills += 1;
+        //debuglog
         Debug.Log("Player has" + PlayerKills + "Kills");
     }
         
