456c7b64baa4e5e558f91e348d74b757fcae90f6
S10206112 <73154832+S10206112@users.noreply.github.com>
2021-08-14 04:18:33 +0800

    a

--- Assets/Scripts/BlockClashSound.cs
+++ Assets/Scripts/BlockClashSound.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class BlockClashSound : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource blocksfalling;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,9 @@ public class BlockClashSound : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        blocksfalling.Play();
     }
 }
